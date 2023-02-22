69981396275eaf5bcdd53729726faafdc9ef3a29
mhking20 <www.taimoorking20@gmail.com>
2023-02-22 15:40:48 +0500

    hh

--- src/Components/AccountContainer.js
+++ src/Components/AccountContainer.js
@@ -1,37 +1,35 @@
 import React from "react";
+import './Styles/Acount.css'
 import { Link } from "react-router-dom";
 function AccountContainer() {
   return (
-    <div className="container-fluid bg-dark d-flex justify-content-center align-items-center p-5">
-      <tabel className="bg-info p-5">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Update</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Your ID</td>
-            <td>Your Name</td>
-            <td>Your Email</td>
-            <td>
-              <Link to='/updateaccount'>
-                {" "}
-                <button className="btn-primary btn">Update Account</button>
-              </Link>
-            </td>
-            <td>
-              <Link to='/deleteaccount'>
-                <button className="btn-danger btn">Delete Account</button>
-              </Link>{" "}
-            </td>
-          </tr>
-        </tbody>
-      </tabel>
+    <div className="container-fluid text-light d-flex justify-content-center align-items-center p-5 account_container">
+      <div className="mb-5 p-5 bg-dark">
+        <div>
+          <h1>Your ID</h1>
+          <h4>Your Current ID</h4>
+        </div>
+        <div>
+          <h1>Your ID</h1>
+          <h4>Your Current ID</h4>
+        </div>
+        <div>
+          <h1>Your ID</h1>
+          <h4>Your Current ID</h4>
+        </div>
+        <div>
+          <h1>Your ID</h1>
+          <h4>Your Current ID</h4>
+        </div>
+        <div>
+          <Link>
+            <button className="btn btn-primary">Update</button>
+          </Link>
+          <Link>
+            <button className="btn btn-danger">Delete</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
